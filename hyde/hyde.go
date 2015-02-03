24029a33679db13d69b768c4d0159a4992337ed6
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-02-03 17:52:32 +0000

    Modify

--- hyde/hyde.go
+++ hyde/hyde.go
@@ -4,10 +4,10 @@ type HydeFunc func(float64) float64
 
 const hydeUnit = 156.0
 
-func ToHyde(v float64) float64 {
+func FromHyde(v float64) float64 {
 	return v * hydeUnit
 }
 
-func FromHyde(v float64) float64 {
+func ToHyde(v float64) float64 {
 	return v / hydeUnit
 }
--- hyde/hyde_test.go
+++ hyde/hyde_test.go
@@ -5,39 +5,39 @@ import (
 	"testing"
 )
 
-func TestToHyde(t *testing.T) {
-	var (
-		result   float64
-		expected float64
-	)
-
-	expected = 156.0
-	result = ToHyde(1.0)
-	if !reflect.DeepEqual(result, expected) {
-		t.Fatalf("Expected %v, but %v:", expected, result)
-	}
-
-	expected = 199.68
-	result = ToHyde(1.28)
-	if !reflect.DeepEqual(result, expected) {
-		t.Fatalf("Expected %v, but %v:", expected, result)
-	}
-}
-
 func TestFromHyde(t *testing.T) {
 	var (
 		result   float64
 		expected float64
 	)
 
+	expected = 156.0
+	result = FromHyde(1.0)
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatalf("Expected %v, but %v:", expected, result)
+	}
+
+	expected = 199.68
+	result = FromHyde(1.28)
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatalf("Expected %v, but %v:", expected, result)
+	}
+}
+
+func TestToHyde(t *testing.T) {
+	var (
+		result   float64
+		expected float64
+	)
+
 	expected = 1.0
-	result = FromHyde(156.0)
+	result = ToHyde(156.0)
 	if !reflect.DeepEqual(result, expected) {
 		t.Fatalf("Expected %v, but %v:", expected, result)
 	}
 
 	expected = 1.28
-	result = FromHyde(199.68)
+	result = ToHyde(199.68)
 	if !reflect.DeepEqual(result, expected) {
 		t.Fatalf("Expected %v, but %v:", expected, result)
 	}
